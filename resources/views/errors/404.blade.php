bae54c525da21fa64fb8f0617d866f19c8df98d9
George Peppard <george@inventor02.me>
2017-03-12 17:06:58 +0000

    404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+@extends("layouts.layout")
+
+@section("title")
+    404
+@endsection
+
+@section("content")
+    <div class="section no-pad-bot" id="index-banner">
+        <br><br>
+        <h1 class="header center orange-text">
+            404 - Let's talk about that
+        </h1>
+        <div class="row center">
+            <h5 class="header col s12 light">
+                The page you were looking for could not be found! Maybe it's moved, or it could just be hiding. Sorry
+                about that.
+            </h5>
+        </div>
+
+        <div class="row center">
+            <a href="/" class="waves-effect waves-light btn-large">Go home</a>
+        </div>
+    </div>
+@endsection
